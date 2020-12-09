a5b467d4211503e108141fdf5fe20f0910ec9f8f
slugskickass <a.cadby@sheffield.ac.uk>
2020-12-09 15:57:29 +0000

    Added extra

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -12,13 +12,17 @@ class MainView : View("Stocks") {
 
     private val stocks = FXCollections.observableArrayList<Stock>(
         Stock(1, "BP", 123.1, 23.5),
-        Stock(2, "DLG", 123.1, 23.5),
-        Stock(3, "PFE", 123.1, 23.5)
+        Stock(2, "CLIG", 123.1, 23.5),
+        Stock(3, "DLG", 123.1, 23.5),
+        Stock(4, "EVR", 123.1, 23.5),
+        Stock(5, "FIS", 123.1, 23.5),
+        Stock(6, "IAG", 123.1, 23.5),
+        Stock(7, "PFE", 123.1, 23.5)
     )
 
 
     override val root =  tableview(stocks) {
-            isEditable = true
+        isEditable = true
         readonlyColumn("ID",Stock::id)
         readonlyColumn("Name",Stock::name)
         readonlyColumn("Start Value",Stock::start_val)
