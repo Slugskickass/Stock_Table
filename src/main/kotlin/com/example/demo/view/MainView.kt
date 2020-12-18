bb1e96eeb414485e2e23049640ad77431f7e0c22
slugskickass <a.cadby@sheffield.ac.uk>
2020-12-18 12:54:49 +0000

    Added piechart

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -2,11 +2,15 @@ package com.example.demo.view
 
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
+import javafx.collections.ListChangeListener
+import javafx.collections.ObservableList
+import javafx.scene.chart.PieChart
 import javafx.scene.paint.Color
 import tornadofx.*
 import yahoofinance.YahooFinance
 import yahoofinance.Stock
 import java.math.BigDecimal
+import javax.swing.event.ChangeListener
 
 private fun return_price(name: String): Double? {
     val stocker = YahooFinance.get(name)?.quote?.price?.toDouble()
@@ -18,14 +22,24 @@ private fun return_currency(name: String): String? {
     return st_curr
 }
 
+val dollartopound = 0.7
+
 class MainView : View("Stocks") {
 
-    class Stock(id: Int, name: String,  number_held: Int, stock_currency: String, start_val: Double, current_val: Double, current_return: Double) {
+
+    class Stock(
+        id: Int,
+        name: String,
+        number_held: Int,
+        stock_currency: String,
+        start_val: Double,
+        current_val: Double,
+        current_return: Double,
+        current_holding: Double
+    ) {
         var id by property(id)
         fun idProperty() = getProperty(Stock::id)
 
-        //    var name by property(name)
-        //    fun nameProperty() = getProperty(Stock::name)
         private val nameProperty = SimpleStringProperty(name)
         fun nameProperty() = nameProperty
         var name by nameProperty
@@ -45,18 +59,29 @@ class MainView : View("Stocks") {
         var current_return by property(current_return)
         fun current_returnProperty() = getProperty(Stock::current_return)
 
+        var current_holding by property(current_holding)
+        fun current_holdingProperty() = getProperty(Stock::current_holding)
+
+
     }
 
+    public var stocks = FXCollections.observableArrayList<Stock>(
+        Stock(1, "BP.L", 100, "GBP", 238.90, 1.0, 1.0, 10.0),
+        Stock(2, "CLIG.L", 100, "GBP", 439.50, 1.0, 1.0, 10.0),
+        Stock(3, "EVR.L", 100, "GBP", 399.540, 1.0, 1.0, 10.0),
+        Stock(4, "FIS", 3, "GBP", 151.8057, 1.0, 1.0, 10.0),
+        Stock(5, "IAG.L", 100, "GBP", 154.54, 1.0, 1.0, 10.0),
+        Stock(6, "PFE", 7, "GBP", 36.880, 1.0, 1.0, 10.0)
+    )
 
-    public val stocks = FXCollections.observableArrayList<Stock>(
-        Stock(1, "BP.L", 1,"GBP", 238.90, 1.0, 1.0),
-        Stock(2, "CLIG.L", 1,"GBP", 439.50, 1.0, 1.0),
-        Stock(3, "EVR.L",  1,"GBP", 399.540, 1.0, 1.0),
-        Stock(4, "FIS",  1,"GBP",151.8057, 1.0, 1.0),
-        Stock(5, "IAG.L",  1,"GBP",154.54, 1.0, 1.0),
-        Stock(6, "PFE", 1,"GBP", 36.880, 1.0, 1.0)
-    )//return_price("BP.L" ?:0.0),
+    public val piedata = stocks.map { item -> PieChart.Data(item.name, item.current_holding) }.observable()
 
+    init {
+        stocks.addListener(ListChangeListener { change ->
+            piedata.clear()
+            stocks.forEach { piedata.add(PieChart.Data(it.name, it.current_holding)) }
+        })
+    }
 
     override val root =  vbox{
 
@@ -65,7 +90,11 @@ class MainView : View("Stocks") {
                 textFill = Color.RED
                 action { button_pressed1() }
             }
-            button("Press Me") {
+            button("Delete Row") {
+                textFill = Color.GREEN
+                action { button_pressed2() }
+            }
+            button("Add Row") {
                 textFill = Color.GREEN
                 action { button_pressed2() }
             }
@@ -74,49 +103,58 @@ class MainView : View("Stocks") {
 
         tableview(stocks) {
             isEditable = true
-            column("ID", Stock::idProperty).makeEditable()
-            column("Name", Stock::nameProperty).makeEditable()
+            column("ID", Stock::idProperty)
+            column("Name", Stock::nameProperty).makeEditable().useTextField()
             column("Number held", Stock::number_heldProperty).makeEditable()
-            column("Currency", Stock::stock_currencyProperty).makeEditable()
+            column("Currency", Stock::stock_currencyProperty)
             column("Start Value", Stock::start_valProperty).makeEditable()
-            column("Current Value", Stock::current_valProperty).makeEditable()
-            column("Current return", Stock::current_returnProperty).makeEditable().cellFormat {
+            column("Current Value", Stock::current_valProperty)
+            column("Current holding", Stock::current_holdingProperty)
+            column("Current return", Stock::current_returnProperty).cellFormat {
                 text = it.toString()
                 style {
                     if (it < 1) {
                         textFill = Color.RED
+                     //   backgroundColor += c("#000010")
+                        backgroundColor += (Color.LIGHTGRAY)
                     } else {
                         textFill = Color.GREEN
                     }
                 }
             }
+
         }
- //       piechart("Value") {
- //           for (current in stocks){
- //               data(current.name, 10.0)
- //           }
-//        }
 
-
-
+        piechart("Current Retun", piedata)
     }
+
     private fun button_pressed1(){
         for (current in stocks){
             current.current_val = return_price(current.name) ?: 0.0
             current.stock_currency = return_currency(current.name) ?: "NA"
             current.current_return = 100 * (( current.current_val.toDouble() / current.start_val.toDouble()) -1 )
-    //        print(current.name)
-    //        print(' ')
-    //        println(current.current_val.toDouble() / current.start_val.toDouble() )
-
+            current.current_holding = current.number_held * current.current_val
+            if (current.stock_currency == "GBp" ){current.current_holding =current.current_holding / 100}
+            if (current.stock_currency == "USD" ){current.current_holding =current.current_holding * dollartopound}
         }
     }
+
     private fun button_pressed2(){
-        for (current in stocks){
+   //     val stocksMap = stocks.map { item -> item.name
+   //     stocks.}
+    val new_stocks = stocks
+        for (current in new_stocks){
             current.current_val = return_price(current.name) ?: 0.0
             current.stock_currency = return_currency(current.name) ?: "NA"
+            current.current_return = 100 * (( current.current_val.toDouble() / current.start_val.toDouble()) -1 )
+            current.current_holding = current.number_held * current.current_val
+            if (current.stock_currency == "GBp" ){current.current_holding =current.current_holding / 100}
+            if (current.stock_currency == "USD" ){current.current_holding =current.current_holding * dollartopound}
+        }
+        stocks.clear()
+        stocks = new_stocks
         }
     }
-}
+
 
 
